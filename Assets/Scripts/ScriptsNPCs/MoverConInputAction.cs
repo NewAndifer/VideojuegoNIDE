f3c09f88fe3fc915df10254a5255c5c71259e149
A01753587 <deliasvaraya@gmail.com>
2026-04-22 20:35:50 -0600

    Ya camina GG ez
    Animations on character

--- Assets/Scripts/ScriptsNPCs/MoverConInputAction.cs
+++ Assets/Scripts/ScriptsNPCs/MoverConInputAction.cs
@@ -1,27 +1,71 @@
+// using UnityEngine;
+// using UnityEngine.InputSystem; 
+
+// public class Mover4Direcciones : MonoBehaviour
+// {
+//     [SerializeField] 
+//     private InputAction accionMover; 
+
+//     [SerializeField] 
+//     private float velocidad = 5f; 
+    
+//     void Start()
+//     {
+//         accionMover.Enable();
+//     }
+
+//     void Update()
+//     {
+//         Vector2 direccion = accionMover.ReadValue<Vector2>();
+//         transform.position = CalcularNuevaPosicion(transform.position, direccion, velocidad, Time.deltaTime);
+//     }
+
+//     public Vector2 CalcularNuevaPosicion(Vector2 posicionActual, Vector2 direccionInput, float vel, float deltaTiempo)
+//     {
+//         return posicionActual + (direccionInput * vel * deltaTiempo);
+//     }
+// }
+
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 public class Mover4Direcciones : MonoBehaviour
 {
-    [SerializeField] 
-    private InputAction accionMover; 
+    public InputAction accionMover; 
+    public float velocidadX = 5f;
+    public float velocidadY = 5f;
 
-    [SerializeField] 
-    private float velocidad = 5f; 
-    
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         accionMover.Enable();
+
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        Vector2 direccion = accionMover.ReadValue<Vector2>();
-        transform.position = CalcularNuevaPosicion(transform.position, direccion, velocidad, Time.deltaTime);
+        Vector2 movimiento = accionMover.ReadValue<Vector2>();
+
+        rb.linearVelocityX = movimiento.x * velocidadX; 
+        rb.linearVelocityY = movimiento.y * velocidadY; 
+
+        ActualizarAnimaciones(movimiento);
     }
 
-    public Vector2 CalcularNuevaPosicion(Vector2 posicionActual, Vector2 direccionInput, float vel, float deltaTiempo)
+    private void ActualizarAnimaciones(Vector2 movimiento)
     {
-        return posicionActual + (direccionInput * vel * deltaTiempo);
+        float velocidadActual = rb.linearVelocity.magnitude;
+        animator.SetFloat("velX", movimiento.x); 
+        animator.SetFloat("velY", movimiento.y); 
+        animator.SetFloat("velocidad", movimiento.magnitude);
+
+        //if (movimiento.x < -0.1f) spriteRenderer.flipX = true;
+        //else if (movimiento.x > 0.1f) spriteRenderer.flipX = false;
     }
 }
